972efa1c7959367ce3b383a5e528895d04b19601
Greg Dobler <greg.dobler@gmail.com>
2017-10-01 17:14:51 -0400

    updated to run from command line

--- py/get_light_curves.py
+++ py/get_light_curves.py
@@ -11,118 +11,83 @@ import pandas as pd
 import scipy.ndimage.measurements as ndm
 from scipy.misc import imread
 
-# if __name__ == "__main__":
-#
-# t0  = time.time()
-# ind = int(sys.argv[1])
+def get_filelist(st, en):
+    """ Get the file list between start and end times. """
 
-# -- parse the list of days
-st   = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d")
-en   = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d")
-#st   = datetime.datetime(2017, 4, 1)
-#en   = datetime.datetime(2017, 5, 1)
-dirs = np.array(sorted([os.path.basename(i) for i in glob.glob( \
-                os.path.join(os.environ["AUDUBON_DATA"], "2017*_night"))]))
-dirt = np.array([datetime.datetime.strptime(i, "%Y-%m-%d_night") 
-                 for i in dirs])
-dirs = dirs[(dirt >= st) & (dirt < en)]
+    # -- get directory list
+    dirs = np.array(sorted([os.path.basename(i) for i in glob.glob( \
+                    os.path.join(os.environ["AUDUBON_DATA"], "2017*_night"))]))
 
+    # -- pull dates off of directory names
+    dirt = np.array([datetime.datetime.strptime(i, "%Y-%m-%d_night") 
+                     for i in dirs])
 
-# -- get the image file list
-fname = os.path.join("..", "data", "2017-04-09_night", 
-                     "1850_d6_1491803544.png")
-nobs  = 1
+    # -- sub-select directories
+    dirs = dirs[(dirt >= st) & (dirt < en)]
 
-# -- read in the bbls
-bbls = np.rot90(np.load("../output/bbls_test.npy").astype(int))
+    # -- return the image file list
+    bpath = os.environ["AUDUBON_DATA"]
 
-# -- read image
-print("FIX THE DIMENSIONS!!!")
-img = imread(fname)[5::2, 1::2]
-
-# -- get the list of bbls
-blist = np.unique(bbls[bbls > 0])
-
-# -- get the building luminosities
-lum = ndm.mean(img, bbls, blist)
+    return np.array([i for j in dirs for i in 
+                     glob.glob(os.path.join(bpath, j, "*d6*.png"))])
 
 
-brimg = np.zeros(bbls.shape)
+if __name__ == "__main__":
 
-for ii in range(blist.size):
-    brimg[bbls == blist[ii]] = lum[ii]
+    t0  = time.time()
+    
+    # -- get the file list
+    ind  = int(sys.argv[1])
+    st    = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d")
+    en    = datetime.datetime.strptime(sys.argv[3], "%Y-%m-%d")
+    #st    = datetime.datetime(2017, 4, 1)
+    #en    = datetime.datetime(2017, 5, 1)
+    flist = get_filelist(st, en)
+    nobs  = flist.size
+    
+    
+    # -- read in the bbls
+    bbls  = np.rot90(np.load("../output/bbls_test.npy").astype(int))
+    blist = np.sort(np.unique(bbls[bbls > 0]))
+    nbbl  = len(blist)
+    
+    
+    # -- set the ouput file name and initialize the log
+    oname = os.path.join("..", "output", "light_curves_d6_{0:04}.npy" \
+                             .format(ind))
+    lname = os.path.join("..", "output", "light_curves_d6_{0:04}.log" \
+                             .format(ind))
+    lopen = open(lname, "w")
+    lopen.write("Extracting lightcurves for {0} observations...\n========\n" \
+                    .format(nobs))
+    
+    
+    # -- initialize the lightcurve array
+    lcs = np.zeros((nobs, nbbl), dtype=float) - 9999
+    
+    
+    # -- loop over observations
+    lopen.write("  FIX THE DIMENSIONS!!!\n")
+    
+    for ii, fname in enumerate(flist):
+        if ii % 10 == 0:
+            lopen.write("  obs {0} of {1}\n".format(ii, nobs))
+            lopen.flush()
+    
+        # - get luminosities
+        img    = imread(fname)[5::2, 1::2]
+        lcs[ii] = ndm.mean(img, bbls, blist)
 
-
-# -- set the ouput file name and initialize the log
-oname = os.path.join("..", "output", "light_curves.npy".format(ind))
-lname = os.path.join("..", "output", "light_curves.log".format(ind))
-lopen = open(lname, "w")
-lopen.write("Extracting lightcurves for {0} observations...\n========\n" \
-                .format(nobs))
-
-
-# -- initialize lightcurve array
-lcs  = np.zeros((nobs, nlab), dtype=float) - 9999
-
-
-# -- utilities
-deg2rad = np.pi / 180.
-nro2    = nrow // 2
-nco2    = ncol // 2
-
-
-# -- initialize the rows and columns grids (the nrow//2 and ncol//2
-#    performs rotation about the center of the image)
-cgr, rgr = np.meshgrid(range(ncol), range(nrow))
-rlabs    = rgr[srcs] - nro2
-clabs    = cgr[srcs] - nco2
-llabs    = labs[0][srcs]
-rot      = np.zeros_like(labs[0])
-
-
-# -- read in image
-for ii in range(nobs):
-
-    if ii % 10 == 0:
-        lopen.write("  obs {0} of {1}\n".format(ii, nobs))
-        lopen.flush()
-
-    if reg.iloc[ii].drow == -9999:
-        continue
-
-    rot[...] = 0.
-    rec      = reg.iloc[ii]
-    ct       = np.cos(-rec.dtheta * deg2rad)
-    st       = np.sin(-rec.dtheta * deg2rad)
-    img      = read_raw(rec.fpath, rec.fname)
-
-    # -- rotate the source labels (the nrow//2 and ncol//2 performs
-    #    rotation about the center of the image)
-    rsrc = (rlabs * ct - clabs * st - rec.drow + nro2).round().astype(int)
-    csrc = (rlabs * st + clabs * ct - rec.dcol + nco2).round().astype(int)
-    gind = (rsrc >= 0) & (rsrc < nrow) & (csrc >= 0) & (csrc < ncol)
-    rsrc = rsrc[gind]
-    csrc = csrc[gind]
-    lsrc = llabs[gind]
-
-    rot[rsrc, csrc] = lsrc
-
-    # -- get brightnesses
-    lun = np.unique(lsrc)
-    lum = np.array([ndm.mean(img[..., i], rot, lun) for i in [0, 1, 2]]).T
-
-    # -- set indices of extracted sources to their values
-    lcs[ii, lun - 1] = lum
-
-    # -- periodically write to file
-    if (ii + 1) % 100 == 0:
-        np.save(oname, lcs[:ii])
-
-# -- write to file
-lopen.write("\nWriting to npy...\n========\n")
-lopen.flush()
-np.save(oname, lcs)
-lopen.write("FINISHED in {0}s\n".format(time.time() - t0))
-lopen.flush()
-lopen.close()
+        # - periodically write to file
+        if (ii + 1) % 100 == 0:
+            np.save(oname, lcs[:ii])
+    
+    
+    # -- write to file
+    lopen.write("\nWriting to npy...\n========\n")
+    lopen.flush()
+    np.save(oname, lcs)
+    lopen.write("FINISHED in {0}s\n".format(time.time() - t0))
+    lopen.flush()
+    lopen.close()
 
